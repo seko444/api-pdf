69e11d93f5f40a60e9f261c5b284a4d3c946f06a
seko444 <cuentasbcfifa777@gmail.com>
2025-06-27 21:31:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from fpdf import FPDF
-import os
 
 app = FastAPI()
 
